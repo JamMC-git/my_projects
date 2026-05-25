c4a2fa38160378271b8abe357dd681a37ef36c39
JamMC-git <j.matthew.corum@gmail.com>
2026-05-25 07:56:03 -0500

    Uploaded first batch of AlgoMaster Algorithm Practice

---
+++ DataAlgorithms/MoveZeros.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution 
+{
+    public:
+        void moveZeroes(vector<int>& nums) 
+        {
+
+           int initialPosition=0;
+
+           for(int i=0; i<nums.size(); i++)
+           {
+               if(nums[i] != 0)
+               {
+                   swap(nums[initialPosition],nums[i]);
+                   initialPosition++;
+               }
+
+           }
+
+      }
+};
+
+
