3090b5480cff253ee4598b7ce6f8f14197ad3e9d
JamMC-git <j.matthew.corum@gmail.com>
2026-05-31 10:21:19 -0500

    Completed Enum.cpp

---
+++ DataAlgorithms/RemoveDuplicatesArrayII.cpp
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public:
+       int removeDuplicates(vector<int>& nums) 
+       {
+               if(nums.size() > 2)
+            {
+                int writePos = 2;
+                for(int i = 2; i < nums.size(); i++)
+                {
+                    if(nums[i] != nums[writePos-2])
+                    {
+                        nums[writePos]=nums[i];
+                        writePos++;
+                    }
+                }
+            }
+               return writePos;
+       }
+};
